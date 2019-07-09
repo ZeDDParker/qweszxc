f6705848eb648a99bb313e88e210c1ddf48529bf
ZeDD Parker <50535201+ZeDDParker@users.noreply.github.com>
2019-07-09 12:30:03 +0700

    Update cekto.py

--- sec/cekto.py
+++ sec/cekto.py
@@ -1,8 +1,8 @@
 try:
-		token = open("log.txt","r").read()
-	except IOError:
-		os.system("rm -rf log.txt")
-		print
-		print tutup+"           ["+merah+"!"+tutup+"] Token not found"
-		print tutup+"           ["+merah+"!"+tutup+"] You must log'in again"
-		login()
+	token = open("log.txt","r").read()
+except IOError:
+	os.system("rm -rf log.txt")
+	print
+	print tutup+"["+merah+"!"+tutup+"] Token not found"
+	print tutup+"["+merah+"!"+tutup+"] You must log'in again"
+	login()
